3f96f504a9fa5a6d4b1d69b49cb88e82a893bcc2
la2ure <2avlaure@gmail.com>
2023-11-15 22:43:06 +0100

    Encrypted Using Vigenere Cipher Method

--- caesarCipher.py
+++ caesarCipher.py
@@ -2,24 +2,26 @@ import unittest
 import string
 
 
-def encrypt(message):
+# Create a function to encrypt a string
+def encrypt(message, key):
     abc = string.ascii_letters + string.punctuation + string.digits + " "
+    key = key * (len(message) // len(key)) + key[: len(message) % len(key)]
     encrypted_message = "".join(
         [
-            abc[abc.find(char) + 1] if len(abc) > (abc.find(char) + 1) else abc[0]
+            abc[(abc.find(char) + abc.find(key[idx])) % len(abc)]
+            # Enumerate iterates on every character to stock both character and its index
             for idx, char in enumerate(message)
         ]
     )
+
     print(encrypted_message)
     return encrypted_message
 
 
 class TestEncryption(unittest.TestCase):
     def setUp(self):
-        self.my_message = "Hello word !?.;8"
-        # output should return : Ifmmpaxpsea"@/<9
+        self.my_message = "shake"
 
-    # Test go here
     def test_inputExist(self):
         self.assertIsNotNone(self.my_message)
 
@@ -27,27 +29,20 @@ class TestEncryption(unittest.TestCase):
         self.assertIsInstance(self.my_message, str)
 
     def test_functionReturnSomething(self):
-        self.assertIsNotNone(encrypt(self.my_message))
+        self.assertIsNotNone(encrypt(self.my_message, "milk"))
 
     def test_lenIO(self):
-        self.assertEqual(len(self.my_message), len(encrypt(self.my_message)))
+        self.assertEqual(len(self.my_message), len(encrypt(self.my_message, "milk")))
 
     def test_differentIO(self):
-        self.assertNotIn(self.my_message, encrypt(self.my_message))
+        self.assertNotIn(self.my_message, encrypt(self.my_message, "milk"))
 
     def test_outputType(self):
-        self.assertIsInstance(encrypt(self.my_message), str)
+        self.assertIsInstance(encrypt(self.my_message, "milk"), str)
 
     def test_shiftedCipher(self):
-        abc = string.ascii_letters + string.punctuation + string.digits + " "
-        encrypted_message = "".join(
-            [
-                abc[abc.find(char) + 1] if len(abc) > (abc.find(char) + 1) else abc[0]
-                for idx, char in enumerate(self.my_message)
-            ]
-        )
-        print(encrypted_message)
-        self.assertEqual(encrypted_message, encrypt(self.my_message))
+        encrypted_message = encrypt(self.my_message, "milk")
+        self.assertNotEqual(self.my_message, encrypted_message)
 
 
 if __name__ == "__main__":
