2ad7625e36a57567afaf83d739164e032f560ec3
la2ure <2avlaure@gmail.com>
2023-11-15 11:54:57 +0100

    Indent code

--- caesarCipher.py
+++ caesarCipher.py
@@ -1,21 +1,29 @@
 import unittest
-import string 
+import string
+
 
 def encrypt(message):
-     abc = string.ascii_letters + string.punctuation + string.digits + " "
-     encrypted_message = "".join([abc[abc.find(char) + 1] if len(abc) > (abc.find(char) + 1) else abc[0] for idx, char in enumerate(message)])
-     print(encrypted_message)
-     return encrypted_message 
+    abc = string.ascii_letters + string.punctuation + string.digits + " "
+    encrypted_message = "".join(
+        [
+            abc[abc.find(char) + 1] if len(abc) > (abc.find(char) + 1) else abc[0]
+            for idx, char in enumerate(message)
+        ]
+    )
+    print(encrypted_message)
+    return encrypted_message
+
 
 class TestEncryption(unittest.TestCase):
     def setUp(self):
         self.my_message = "Hello word !?.;8"
         # output should return : Ifmmpaxpsea"@/<9
+
     # Test go here
     def test_inputExist(self):
         self.assertIsNotNone(self.my_message)
- 
-    def test_inputType(self): 
+
+    def test_inputType(self):
         self.assertIsInstance(self.my_message, str)
 
     def test_functionReturnSomething(self):
@@ -25,17 +33,22 @@ class TestEncryption(unittest.TestCase):
         self.assertEqual(len(self.my_message), len(encrypt(self.my_message)))
 
     def test_differentIO(self):
-        self.assertNotIn(self.my_message,encrypt(self.my_message))
+        self.assertNotIn(self.my_message, encrypt(self.my_message))
 
     def test_outputType(self):
         self.assertIsInstance(encrypt(self.my_message), str)
 
     def test_shiftedCipher(self):
         abc = string.ascii_letters + string.punctuation + string.digits + " "
-        encrypted_message = "".join([abc[abc.find(char) + 1] if len(abc) > (abc.find(char) + 1) else abc[0] for idx, char in enumerate(self.my_message)])
+        encrypted_message = "".join(
+            [
+                abc[abc.find(char) + 1] if len(abc) > (abc.find(char) + 1) else abc[0]
+                for idx, char in enumerate(self.my_message)
+            ]
+        )
         print(encrypted_message)
         self.assertEqual(encrypted_message, encrypt(self.my_message))
 
 
-if __name__=="__main__":
-    unittest.main() 
+if __name__ == "__main__":
+    unittest.main()
